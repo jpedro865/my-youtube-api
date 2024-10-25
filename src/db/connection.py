8548b6a44ea0713faaeabc8449e7670774a29130
montei_j <montei_j@etna-alternance.net>
2024-10-25 11:20:30 +0200

    [MAJ] gestion de la connection db pendant que serveur mysql  pas encore en ligne

--- src/db/connection.py
+++ src/db/connection.py
@@ -22,32 +22,38 @@ db_url = "mysql+pymysql://{}:{}@{}:{}/{}".format(
 
 # Function to get a connection to the database
 def get_engine():
-  print("Connecting to database...")
   try:
     engine = create_engine(db_url)
     print("Connection successful")
     return engine
   except Exception as e:
-    print("Connection failed, error:")
+    print("Failed while creating db engine:")
     print(e)
     return None
 
 # Function to get a session to the database using SQLAlchemy ORM
 def get_session():
+  print("Establishing session with db...")
   try:
     session_local = sessionmaker(autocommit=False, autoflush=False, bind=get_engine())
+    print("Connection successful")
     return session_local()
   except Exception as e:
     print("Error loading db session:")
     print(e)
-    return None
+    print("Retrying...")
+    return get_session()
 
+# Function to get the base of the database
 def get_base():
+  print("Establishing base builder with db...")
   try:
     Base = automap_base()
     Base.prepare(get_engine(), reflect=True)
+    print("Connection successful")
     return Base
   except Exception as e:
     print("Error loading db base:")
     print(e)
-    return None
+    print("Retrying...")
+    return get_base()
